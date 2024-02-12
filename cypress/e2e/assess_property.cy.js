5f325119abeac32a0b5d0273beb5b9d19b99bb5f
rgreenan55 <rjgreenan55@gmail.com>
2024-02-12 16:19:04 -0400

    Added and setup basic cypress - still need to add tests

---
+++ cypress/e2e/assess_property.cy.js
@@ -0,0 +1,13 @@
+describe("Test Assessment Request", () => {
+    it("Assess by Address Search", () => {
+        
+    })
+
+    it("Assess by Filters", () => {
+
+    })
+
+    it("Assess by Map Select", () => {
+
+    })
+})
