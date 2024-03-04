dccfbf56cb45fa96e4d44ea7fec0c5f9d84f1889
rgreenan55 <rjgreenan55@gmail.com>
2024-03-04 19:19:42 -0400

    Init

--- src/services/assessment.js
+++ src/services/assessment.js
@@ -5,15 +5,44 @@ const api = axios.create({
     headers: { 'Access-Control-Allow-Origin': '*' }
 });
 
-const GetAssessment = async () => {
+const RequestAIArguments = async () => {
     try {
-        let response = await api.get('/get-test-url');
+        let reponse = await api.get('/get-ai-args');
         let data = response.data;
-    
+
+        // Of the form [{name: 'x', type: 'double'}]
+
         return data;
-    } catch (e) {
+    } catch(e) {
         return null;
     }
 }
 
-export { GetAssessment };
+const GetAssessmentByArguments = async (data) => {
+    try {
+        // Data of the form [{name: 'x', data: 'xyz'}]
+        let response = await api.get('/get-assessment-by-arguments', { params: { arguments: data }});
+        let data = response.data;
+
+        return data;
+    } catch(e) {
+        return null;
+    }
+}
+
+const GetAssessmentByAdresses = async (data) => {
+    try {
+        // Address Data
+        let response = await api.get('/get-assessment-by-address', { params: { arguments: data }});
+
+        return null;
+    } catch(e) {
+        return null;
+    }
+}
+
+export {
+    RequestAIArguments,
+    GetAssessmentByArguments,
+    GetAssessmentByAdresses
+};
