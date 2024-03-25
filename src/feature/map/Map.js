98244a59c9cf581b8fed9e6800f910eb56b2a2f9
rgreenan55 <rjgreenan55@gmail.com>
2024-03-25 16:47:26 -0300

    Fixed map coordinates default

--- src/feature/map/Map.js
+++ src/feature/map/Map.js
@@ -2,13 +2,6 @@ import React from 'react'
 import { Box } from '@mui/material';
 import { MapContainer, TileLayer, ZoomControl, useMap } from 'react-leaflet'
 
-const coordinates = {
-    'Fredericton': [45.96479551374326, -66.64788378405906],
-    'Moncton': [46.14249431518082, -64.71088019896406],
-    'NewBrunswick': [46.93751810993752, -66.21517610891922],
-    'Default': [38.685516, -101.073324]
-}
-
 /* Map Component Utilizing Leaflet */
 const Map = ({ addressInfo }) => {
     const [location, setLocation] = React.useState();
@@ -39,10 +32,9 @@ const Map = ({ addressInfo }) => {
 
     return (
         <Box position='absolute' width='100%' height='100vh' zIndex={1}>
-            <MapContainer center={coordinates.Fredericton} zoom={14} zoomControl={false} ref={setMap}>
+            <MapContainer center={[45.424721, -75.695000]} zoom={12} zoomControl={false} ref={setMap}>
                 <TileLayer url={'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'} subdomains={['mt0','mt1','mt2','mt3']} maxZoom={20} />
                 <ZoomControl position={'bottomright'}/>
-                {/* <Recenter location={location} /> */}
             </MapContainer>
         </Box>
     )
